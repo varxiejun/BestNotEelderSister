3021888ccb616e31deb7aeda96438d1b32d7c2d6
xiejun <273794982@qq.com>
2016-03-28 14:33:21 +0800

    Initial Commit

---
+++ BestNotEelderSister/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BestNotEelderSister
+//
+//  Created by 看楼听雨 on 16/3/28.
+//  Copyright © 2016年 XJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BestNotEelderSister/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  BestNotEelderSister
+//
+//  Created by 看楼听雨 on 16/3/28.
+//  Copyright © 2016年 XJ. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
