8bbbe2915880ea128e383e16c485b6ba50a17d0e
看楼听雨 <273794982@qq.com>
2016-03-30 17:14:08 +0800

    删除无用代码

--- BestNotEelderSister/Comon/LoginViewController.m
+++ BestNotEelderSister/Comon/LoginViewController.m
@@ -30,31 +30,6 @@ metamacro_foreach_cxt(rac_weakify_,, __weak, __VA_ARGS__)
     
     
      NSPredicate *phonePred = [NSPredicate predicateWithFormat:@"SELF MATCHES %@",kRegextestMobile];
-//    @weakify(self)
-//    self.loginBtn.enabled = NO;
-//    self.password.enabled = NO;
-//    [[self.username.rac_textSignal filter:^BOOL(id value) {
-//        if ([phonePred evaluateWithObject:value]) {
-//            self_weak_.password.enabled = YES;
-//        }else{
-//            self_weak_.password.enabled = NO;
-//            self_weak_.password.text = @"";
-//        }
-//        return [value length] == 11;
-//    }] subscribeNext:^(id x) {
-//       
-//        [[self.password.rac_textSignal filter:^BOOL(id value) {
-//            if ([value length] > 5) {
-//                self.loginBtn.enabled = YES;
-//            }else{
-//                self.loginBtn.enabled = NO;
-//            }
-//            return [value length] > 5;
-//        }] subscribeNext:^(id x) {
-//           
-//            XJLog(x);
-//        }];
-//    }];
     // 通过RAC进行模拟登陆
     [[RACSignal combineLatest:@[self.username.rac_textSignal, self.password.rac_textSignal]
       reduce:^(NSString *firstName, NSString *lastName){
